fc7eaa36ddffcf9379609f7c04f0db8cb402f7d9
dannielccw <dannielccw@gmail.com>
2017-03-30 16:13:35 +0800

    + onCreateOptionsMenu

--- app/src/main/java/com/example/yvtc/s033003/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s033003/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.yvtc.s033003;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> data = new ArrayList<>();
+    MyAdapter adapter;
 
     ListView lv;
     @Override
@@ -32,9 +36,33 @@ public class MainActivity extends AppCompatActivity {
         data.add("AA10");
         data.add("AA11");
         data.add("AA12");
-        MyAdapter adapter = new MyAdapter(MainActivity.this, data);
+        adapter = new MyAdapter(MainActivity.this, data);
 
         lv.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Show");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getTitle().equals("Show"))
+        {
+            StringBuilder sb = new StringBuilder();
+            int i;
+            for (i=0;i<adapter.chks.length;i++)
+            {
+                if (adapter.chks[i])
+                {
+                    sb.append(data.get(i) + ",");
+                }
+            }
+            Toast.makeText(MainActivity.this, sb.toString(), Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
